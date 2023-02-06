d491be9bc8142ade0ca2d4b799aafceeabb28ecd
JuanJaramilloSalazar <salazarjdj@gmail.com>
2023-02-06 15:24:09 -0500

    Editar Heroe
    Se implementa Two-way binding mediante ngModel para poder editar la propiedad name del componente Heroe

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { HeroesComponent } from './heroes-page/heroes.component';
 
@@ -14,7 +15,8 @@ import { HeroesComponent } from './heroes-page/heroes.component';
     ],
     imports:
     [
-        CommonModule
+        CommonModule,
+        FormsModule
     ]
 })
 export class HeroesModule {}
