1cbb9575e2d6b1cac5b8192775908e5e06fbd623
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-02-06 16:18:56 -0500

    Implementacion
    Se realiza la implementacion de la clase heroe

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+
+
+export class HeroesComponent {
+
+  heroes = HEROES;
+}
+
+
