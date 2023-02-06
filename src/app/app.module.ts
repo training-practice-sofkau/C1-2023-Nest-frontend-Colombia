dff5af03a4d14de00d8663429b9aea9f30e130fa
JuanVanegas <44687061+lSagenav@users.noreply.github.com>
2023-02-06 14:11:24 -0500

    TITULO: agrego metodo de editar heroe y listar heroes DESCRIPCION: se generan estos dos metodos y se vizualizan vien ambos en el html gerado por angular en el navegador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import { HeroesComponent } from './heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
