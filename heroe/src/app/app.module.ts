84710cf752b2e384e264dbb569e2714c3f708bdc
David Fernando Briceño Galindo <d.bricenogalindo@gmail.com>
2023-02-06 15:14:15 -0500

    Generacion de componentes
    Generacion de componente Heroe por medio del comando ng generate component Heroe

--- heroe/src/app/app.module.ts
+++ heroe/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroeComponent } from './heroe/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule
