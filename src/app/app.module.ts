94efd84c4f3e81a90d0a96e14f35f9e2b272ac89
jmgaviriam <jmgaviriam@gmail.com>
2023-02-06 15:04:52 -0500

    El editor Hero - Se crea un nuevo componente para mostrar la información sobre el héroe y se coloca en el intérprete de comandos de la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+
   ],
   providers: [],
   bootstrap: [AppComponent]
