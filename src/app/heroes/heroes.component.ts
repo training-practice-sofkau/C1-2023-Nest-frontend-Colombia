c39ef654f32bb2a65ab68e098743656a7ede0884
Javier Carrillo <javiercamur98@gmail.com>
2023-02-06 16:19:00 -0500

    Se agreda el modulo heroes
    Se crea el modulo heroes y se reubica la carpeta app.modules

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sofka-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent {
+}
