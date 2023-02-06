a0f9ea93c474052c1c87a4c3f13b95f067a36d15
codeJ30 <juancode09@gmail.com>
2023-02-06 16:50:53 -0500

    TITULO: Update tour hero, DESCRIPCION: Se actualiza el componente hero details y se crea el servicio

--- tours-of-heroes/src/app/app.module.ts
+++ tours-of-heroes/src/app/app.module.ts
@@ -1,20 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-
-
+import { HeroDetailComponent } from './hero-details/hero-details.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule
   ],
   providers: [],
