b2ee66bb8bcf8c234a8ce96bf7109aba2b7977c4
ERuiz <edwar_775@hotmail.com>
2023-02-06 11:29:12 -0500

    Creación del proyecto
     Se genera proyecto de Angular con Prefix sofka-bank, packge manager NPM y Style SCSS con el Routing habilitado (ng new C1-2023-Angular-frontend-Colombia --package-manager npm --prefix sofka-bank --routing true --style scss).

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sofka-bank-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'C1-2023-Angular-frontend-Colombia';
+}
