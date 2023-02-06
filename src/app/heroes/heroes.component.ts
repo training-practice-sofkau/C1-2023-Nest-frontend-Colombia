daa547777ed4e38c7a6c5bade5d96043605a1145
Andres Pardo <carlos.pardo@uptc.edu.co>
2023-02-06 16:48:59 -0500

    mensaje de error - me sale el siguiente mensaje de error A merge conflicted o/src/app/app-routing.module.ts

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 import { MessageService } from '../message.service';
-
+// mensaje de error
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
