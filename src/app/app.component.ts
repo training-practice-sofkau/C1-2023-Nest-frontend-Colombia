65eb573d254cf8f2573bb4aeceed35fe9d3ee1fe
Andres Pardo <carlos.pardo@uptc.edu.co>
2023-02-06 15:47:10 -0500

    editar heroe y mostrar una lista- hasta el momento eh hecho estas dos partes del tutorial

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+// import { Component } from '@angular/core';
+// import { Hero } from './hero';
+// import { HEROES } from './mock-heroes';
+
+// @Component({
+//   selector: 'app-heroes',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./heroes.component.css']
+// })
+
+// export class AppComponent {
+
+//   heroes = HEROES;
+//   selectedHero?: Hero;
+
+//   onSelect(hero: Hero): void {
+//     this.selectedHero = hero;
+//   }
+// }
