30c4d4eaf0369825aec15cc6d1242e3e4a3310f4
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-02-06 15:37:35 -0500

    MODELS Y MOCK - crear carpeta *models* y *mock* guardar todos los modelos y los datos moqueados

--- src/app/services/hero/hero.service.ts
+++ src/app/services/hero/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { Hero } from './hero';
-import { HEROES } from './mock.heroes';
+import { Hero } from '../../models/hero';
+import { HEROES } from '../../mock/mock.heroes';
 //import { MessageService } from './message.service';
 
 @Injectable({
