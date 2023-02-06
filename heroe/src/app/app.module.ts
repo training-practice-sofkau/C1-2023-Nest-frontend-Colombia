a8fc4547e0cc9e0e8921e39b868ccb984cd928f2
David Fernando Briceño Galindo <d.bricenogalindo@gmail.com>
2023-02-06 17:06:39 -0500

    Hero Detail

--- heroe/src/app/app.module.ts
+++ heroe/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeroeComponent } from './heroe/heroe.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroeComponent
+    HeroeComponent,
+    HeroDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- heroe/src/app/heroe/heroe.component.ts
+++ heroe/src/app/heroe/heroe.component.ts
@@ -1,13 +1,20 @@
+import { Hero } from '../hero';
 import { Component, OnInit } from '@angular/core';
+import { Heroes } from '../mock-hero';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styleUrls: ['./heroe.component.css']
+  styleUrls: ['./heroe.component.scss']
 })
 export class HeroeComponent  implements OnInit{
-    hero = 'Windstorm';
+    heroes = Heroes;
+    selectedHero ?: Hero;
     constructor(){}
     ngOnInit() {
+    }
+    onSelect(hero:Hero){
+      this.selectedHero = hero;
+      console.log(this.selectedHero);
     }
 }
