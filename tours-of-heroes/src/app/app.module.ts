90ee2664406a7b064e11a57d5f21ac72e9e12c56
codeJ30 <juancode09@gmail.com>
2023-02-06 20:55:25 -0500

    TITULO: Update Hero-tour punto 4, DESCRIPCION: Se Actualiza proyecto y se crea component message y message service

--- tours-of-heroes/src/app/app.module.ts
+++ tours-of-heroes/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-details/hero-details.component';
+import { MessagesComponent } from './messages/messages.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
