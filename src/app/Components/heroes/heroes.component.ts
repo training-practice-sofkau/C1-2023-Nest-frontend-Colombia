16a9185b24d2b8e50f0f746ca9a22652bed5558f
Jesus <jesusdavidpoloespitia@gmail.com>
2023-02-06 20:00:10 -0500

    Punto 2
    Punto 2 realizado con exito

--- src/app/Components/heroes/heroes.component.ts
+++ src/app/Components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Hero } from 'src/app/Interfaces/hero';
+import { Hero } from 'src/app/interfaces/hero';
+import { HEROES } from 'src/app/mock/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,6 +8,13 @@ import { Hero } from 'src/app/Interfaces/hero';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   hero: Hero = {
     id:1,
     name:'windstorm'
