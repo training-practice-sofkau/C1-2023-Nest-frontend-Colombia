f0077bb220e0abaa94e65cbaa8b582eefea57040
Jesus <jesusdavidpoloespitia@gmail.com>
2023-02-06 20:34:11 -0500

    Punto 3
    El punto 3 ha sido realizado con exito y esta funcional

--- src/app/main/app.module.ts
+++ src/app/main/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from '../main/app.component';
-import { HeroesComponent } from '../Components/heroes/heroes.component';
+import { HeroesComponent } from 'src/app/components/heroes/heroes.component';
+import { HeroDetailComponent } from '../components/hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
