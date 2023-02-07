6f26573bed487d5a21dbb12cb814ef357e930dd5
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-02-07 12:54:24 -0500

    Creacion
    Se realizo la creacion del modulo dasboard  y su funcionamiento

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,7 +7,7 @@ import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
 
