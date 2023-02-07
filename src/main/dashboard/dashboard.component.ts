e4b2b527ed1ec4bd48dafd2c5c1d1bc6c4dc8b84
Javier Carrillo <javiercamur98@gmail.com>
2023-02-07 15:06:11 -0500

    Se agrega la navegacion

--- src/main/dashboard/dashboard.component.ts
+++ src/main/dashboard/dashboard.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../interfaces/hero';
 import { HeroService } from '../services/hero/hero.service';
-import { MessageService } from '../services/message/message.service';
-import { HEROES } from '../mocks/mock-heroes';
 
 @Component({
-  selector: 'sofka-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  selector: 'sofka-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
 })
-export class HeroesComponent implements OnInit {
-
+export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) { }
@@ -21,6 +18,6 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes);
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
 }
