58e70e7b2c423ce54e2f1c18cf089c625c0c93f4
jmgaviriam <jmgaviriam@gmail.com>
2023-02-07 00:20:15 -0500

    Agregando servicios - se crea un HeroService que todas las clases de la aplicación pueden utilizar para obtener héroes. En lugar de crear ese servicio con el método new, usa la inyección de dependencias que soporta Angular para inyectarlo en el constructor HeroesComponent.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -4,8 +4,8 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.scss']
+  styleUrls: ['./hero-detail.component.scss'],
 })
-export class HeroDetailComponent{
+export class HeroDetailComponent {
   @Input() hero?: Hero;
 }
