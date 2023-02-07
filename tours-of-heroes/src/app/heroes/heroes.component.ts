b3499d6ad8bfb9cdfc04828fea0e309d4c8f86c3
codeJ30 <juancode09@gmail.com>
2023-02-06 23:53:27 -0500

    TITULO: Update Tour Hero, DESCRIPCION: Se actualiza el proyecto en su punto 5

--- tours-of-heroes/src/app/heroes/heroes.component.ts
+++ tours-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
 import { MessageService } from '../message.service';
 
@@ -9,14 +9,13 @@ import { MessageService } from '../message.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-
 export class HeroesComponent implements OnInit {
 
-
   selectedHero?: Hero;
+
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -27,10 +26,8 @@ export class HeroesComponent implements OnInit {
     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
-
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
-
-
 }
