ac9562d5d856b8daa7a311d8d74c3455a3a5a496
codeJ30 <juancode09@gmail.com>
2023-02-07 09:49:26 -0500

    TITULO: Update tour-Hero, DESCRIPCION: Se crea un estructura de proyecto por carpetas

--- tours-of-heroes/src/app/app.module.ts
+++ tours-of-heroes/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroDetailComponent } from './hero-details/hero-details.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { HeroDetailComponent } from './component/hero-details/hero-details.component';
+import { HeroesComponent } from './component/heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
-import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HeroSearchComponent } from './component/hero-search/hero-search.component';
 
 @NgModule({
   imports: [
