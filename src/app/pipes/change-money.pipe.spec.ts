3db90f825d42294550b7c1736fc40e0308654a0d
JuanVanegas <44687061+lSagenav@users.noreply.github.com>
2023-02-07 19:52:15 -0500

    TITULO: se crean los pipes restantes DESCRIPCION: se genera la logica de los pipes restantes y el ultimo pipe me queda por solucionar un bug

---
+++ src/app/pipes/change-money.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ChangeMoneyPipe } from './change-money.pipe';
+
+describe('ChangeMoneyPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ChangeMoneyPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
