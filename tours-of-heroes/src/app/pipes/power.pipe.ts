0dc0fad3363c9b18655218f41b1436d2b1470a8f
codeJ30 <juancode09@gmail.com>
2023-02-08 14:30:38 -0500

    TITULO: Updata al pipe power, DESCRIPCION: Se actualiza el pipe , con la implementacion aconsejada por el coach Matias

--- tours-of-heroes/src/app/pipes/power.pipe.ts
+++ tours-of-heroes/src/app/pipes/power.pipe.ts
@@ -6,17 +6,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PowerPipe implements PipeTransform {
 
   transform(powerDate: number): string {
-    if(powerDate > 10 && powerDate <= 30){
-      return 'Debil'
-    }else if (powerDate > 30 && powerDate < 50){
-      return 'Resistente'
-       }else if (powerDate > 50 && powerDate < 75){
-          return 'Fuerte'
-          }else if(powerDate > 75){
-             return 'Poderoso'
-               }else{
-                  return 'Solo es un Humano'
-              }
-
+      const powerLevels =[
+        {limite: 10 , value : 'Solo es un humano'},
+        {limite: 30 , value : 'Debil'},
+        {limite: 50 , value : 'Resistente'},
+        {limite: 75 , value : 'Fuerte'},
+        {limite: Infinity , value : 'Poderoso' },
+      ];
+      return powerLevels.find(level=> powerDate <= level.limite)!.value;
   }
 }
