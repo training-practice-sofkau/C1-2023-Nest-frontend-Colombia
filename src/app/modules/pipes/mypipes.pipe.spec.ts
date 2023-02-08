ee1abf79c0a6218d63c0fa3a100f6e850de8d071
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-02-07 23:22:18 -0500

    Creacion y ordenamiento
    Se realizo la creacion de los respectivos modulos o paquetes para un buen manejo de los pipes

--- src/app/modules/pipes/mypipes.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MypipesPipe } from './mypipes.pipe';
-
-describe('MypipesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MypipesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
