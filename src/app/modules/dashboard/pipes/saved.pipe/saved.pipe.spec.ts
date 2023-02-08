7ee0bd0f4acf2f1ee0f53e0b8baf904445fc6db6
migueltorresd <migueltorresdiaz1992@gmail.com>
2023-02-07 22:58:46 -0500

    Tituto: se limpian carpetas  -Descripcion: se limpian carpetas para iniciar con el proyecto frontend

--- src/app/modules/dashboard/pipes/saved.pipe/saved.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SavedPipe } from './saved.pipe';
-
-describe('SavedPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SavedPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
