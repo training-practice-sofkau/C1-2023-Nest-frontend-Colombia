f2d7e5c3e13dc4a60cca9621db3a2ef15d50aaac
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2023-02-09 16:55:10 -0500

    Se realizan cambios en los headers
    Se realiza cambios en los headers y ya deja dependientes a una variable booleana.

--- src/app/modules/main/components/header/header.component.ts
+++ src/app/modules/main/components/header/header.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  login:boolean
+  constructor() { 
+    this.login=true
+  }
 
   ngOnInit(): void {
   }
