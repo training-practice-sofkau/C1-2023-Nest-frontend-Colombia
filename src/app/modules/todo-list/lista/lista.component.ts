2dd6895fab5f9c5e08b31b20fcd01a23a141433b
Javier Carrillo <javiercamur98@gmail.com>
2023-02-09 16:10:23 -0500

    Se agrega el componente lista
    Componente que servirá para una vista donde se ve la lista de los TodoList

---
+++ src/app/modules/todo-list/lista/lista.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sofka-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.scss']
+})
+export class ListaComponent {
+  agregar: string[];
+  editar: string[];
+  eliminar: string[];
+  detalles: string[];
+
+constructor(){
+  this.editar = ["editar"];
+  this.agregar = ["agregar"];
+  this.eliminar = ["agregar"];
+  this.detalles = ["agregar"];
+}
+
+}
