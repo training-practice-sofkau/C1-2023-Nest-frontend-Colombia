edef7699edfbe0fcff0f007937776bdf326196ee
Julian362 <Juliangarcia362@hotmail.com>
2023-02-09 16:38:58 -0500

    se agrega maquetacion
    se agrega la maquetación de transfer

--- src/app/modules/movements/movements.module.ts
+++ src/app/modules/movements/movements.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovementsRoutingModule } from './movements-routing.module';
+import { HistoryComponent } from './components/history/history.component';
+import { MovementsComponent } from './pages/movements/movements.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MovementsComponent,HistoryComponent
+  ],
   imports: [
     CommonModule,MovementsRoutingModule
   ]
