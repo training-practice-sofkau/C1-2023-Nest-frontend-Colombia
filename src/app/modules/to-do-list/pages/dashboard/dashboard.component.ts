f54bec444d39d72fd67996cada5e4e2b5e18ff76
Kevin Baquero <baquerochavarro@gmail.com>
2023-02-09 18:18:21 -0500

    Updating dashboard component
    updating dashboard component with routes

--- src/app/modules/to-do-list/pages/dashboard/dashboard.component.ts
+++ src/app/modules/to-do-list/pages/dashboard/dashboard.component.ts
@@ -8,13 +8,19 @@ import { Component } from '@angular/core';
 export class DashboardComponent {
   days: string[];
   toDoList: string[];
-  routeDetalle: string[];
   routeDashboard: string[];
+  routeDetail: string[];
+  routeAdd: string[];
+  routeEdit: string[];
+  routeDelete: string[];
 
   constructor() {
     this.days = ['1', '2', '3', '4'];
-    this.toDoList = ['A'];
-    this.routeDetalle = ['detalle'];
+    this.toDoList = ['A', 'B'];
     this.routeDashboard = ['../'];
+    this.routeDetail = ['detail', 'id']; //le envio el id
+    this.routeAdd = ['add'];
+    this.routeEdit = ['edit', 'id'];
+    this.routeDelete = ['delete', 'id'];
   }
 }
