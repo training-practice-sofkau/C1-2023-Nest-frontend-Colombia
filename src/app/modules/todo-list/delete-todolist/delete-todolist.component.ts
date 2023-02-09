5291ee3892bcae5625de3d26e9e91b51345c9e16
Javier Carrillo <javiercamur98@gmail.com>
2023-02-09 16:20:39 -0500

    Se agrega el componente delete-todolist
    Componente que servirá para mostrar una vista para eliminar un todoList

---
+++ src/app/modules/todo-list/delete-todolist/delete-todolist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sofka-delete-todolist',
+  templateUrl: './delete-todolist.component.html',
+  styleUrls: ['./delete-todolist.component.scss']
+})
+export class DeleteTodolistComponent {
+
+}
