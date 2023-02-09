68afc2b7d0dd35aa07223937545e5662b631064a
Javier Carrillo <javiercamur98@gmail.com>
2023-02-09 16:19:40 -0500

    Se agrega el componente detail-todolist
    Componente que servirá para mostrar una vista con el detalle de un todoList

---
+++ src/app/modules/todo-list/detail-todolist/detail-todolist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sofka-detail-todolist',
+  templateUrl: './detail-todolist.component.html',
+  styleUrls: ['./detail-todolist.component.scss']
+})
+export class DetailTodolistComponent {
+
+}
