552aa59dbf3e59df7d231e63447007b191b462ae
David Fernando Briceño Galindo <d.bricenogalindo@gmail.com>
2023-02-09 00:14:30 -0500

    Creacion proyecto nuevo
    Se realiza la creacion del proyecto para la api

---
+++ Calendar_TodoList/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './plantillas/header/header.component';
+import { FooterComponent } from './plantillas/footer/footer.component';
+import { LoginComponent } from './vistas/login/login.component';
+import { DashboardComponent } from './vistas/dashboard/dashboard.component';
+import { NuevoComponent } from './vistas/nuevo/nuevo.component';
+import { EditarComponent } from './vistas/editar/editar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    DashboardComponent,
+    NuevoComponent,
+    EditarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- heroe/src/app/hero/hero.ts
+++
@@ -1,4 +0,0 @@
-export class Hero{
-  id: number = 0;
-  name: string = "";
-}
