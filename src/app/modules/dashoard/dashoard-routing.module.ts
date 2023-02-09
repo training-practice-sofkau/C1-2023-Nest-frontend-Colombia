6c2c528632623a8981333834f15c770872241724
migueltorresd <migueltorresdiaz1992@gmail.com>
2023-02-09 17:07:00 -0500

    Titulo: se crea componente de account  -Descripcion: Se crea componentes de cuentas

--- src/app/modules/dashoard/dashoard-routing.module.ts
+++ src/app/modules/dashoard/dashoard-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeeComponent } from './pages/homee/homee.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeeComponent,
-    children: [{ path: 'siderbar', component: SidebarComponent }],
+    children: [
+      { path: 'siderbar', component: SidebarComponent },
+      { path: 'accounts', component: AccountsComponent },
+    ],
   },
 ];
 
--- src/app/modules/dashoard/dashoard.module.ts
+++ src/app/modules/dashoard/dashoard.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { DashoardRoutingModule } from './dashoard-routing.module';
 import { HomeeComponent } from './pages/homee/homee.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
 
 @NgModule({
-  declarations: [HomeeComponent, SidebarComponent],
+  declarations: [HomeeComponent, SidebarComponent, AccountsComponent],
   imports: [CommonModule, DashoardRoutingModule],
 })
 export class DashoardModule {}
