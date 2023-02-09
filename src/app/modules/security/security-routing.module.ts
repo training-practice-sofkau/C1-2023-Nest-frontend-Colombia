fa0f25acaf0412e690af916c29f6b182f93e3c3c
Julian362 <Juliangarcia362@hotmail.com>
2023-02-09 16:28:07 -0500

    se maqueta singup
    se maqueta la opción para registrarse

--- src/app/modules/security/security-routing.module.ts
+++ src/app/modules/security/security-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { FormSignupComponent } from './components/form-signup/form-signup.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: FormSignupComponent
   }
 ];
 
--- src/app/modules/security/security.module.ts
+++ src/app/modules/security/security.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { SecurityRoutingModule } from './security-routing.module';
 import { FormComponent } from './components/form/form.component';
 import { LoginComponent } from './pages/login/login.component';
+import { SingupComponent } from './pages/singup/singup.component';
+import { FormSignupComponent } from './components/form-signup/form-signup.component';
 
 
 
 @NgModule({
   declarations: [
-    FormComponent, LoginComponent
+    FormComponent, LoginComponent, SingupComponent, FormSignupComponent
   ],
   imports: [
     CommonModule, SecurityRoutingModule
