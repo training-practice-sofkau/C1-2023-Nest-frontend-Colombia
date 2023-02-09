2e07c3165a96b876f484e0918f4091c3cee4a689
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-02-09 04:04:31 -0500

    Creacion
    Se realizo la creacion del modulo del calendario al igual que la carga perezosa

--- src/app/modules/main/app-routing.module.ts
+++ src/app/modules/main/app-routing.module.ts
@@ -1,6 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+
+  {
+    //carga perezosa
+    path: 'calendar-todo-list',
+    loadChildren: () => import('../calendar-todo-list/calendar-todo-list.module').then(m => m.CalendarTodoListModule)
+
+  },
+  {
+
+
+
+  }
+
+
+];
+
+
+
 
 @NgModule({
   exports: [ RouterModule ]
