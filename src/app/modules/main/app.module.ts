cbc09a449cad9ed33a588cdf5d356d0a76a57cc1
Kevin Baquero <baquerochavarro@gmail.com>
2023-02-12 23:58:11 -0500

    Update to-do-list.module and other files
    Adding HttpClient to the module and other changes

--- src/app/modules/main/app.module.ts
+++ src/app/modules/main/app.module.ts
@@ -6,10 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './pages/app/app.component';
 import { IndexComponent } from './pages/index/index.component';
 import { MyPipePipe } from './pipes/my-pipe/my-pipe.pipe';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, IndexComponent, MyPipePipe],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
