35a2ed15596cb00f03c61a733499c66cbfc887f4
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2023-02-12 21:14:57 -0500

    Se hace mejoras de scss
    Se hace mejoras de scss en la pagina de updateCustomer

--- src/app/modules/user/pages/update-costumer/update-costumer.component.ts
+++ src/app/modules/user/pages/update-costumer/update-costumer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Route } from '@angular/router';
+import Swal from 'sweetalert2';
 import { IDocumenType } from '../../models/document-type.model';
 import { IUpdateUser } from '../../models/update-user.model';
 import { UserModel } from '../../models/user.model';
@@ -36,14 +37,12 @@ export class UpdateCostumerComponent implements OnInit {
     this.id = idNew !== null ? idNew : '';
     this.userService.getUserById(this.id).subscribe({
       next: (data) => {
-        console.log(data)
         this.document = data.document;
         this.fullName = data.fullName;
         this.email = data.email;
         this.phone = data.phone;
         this.password = data.password;
         this.documentTypeId = data.documentType.id
-        console.log("Este es el tipo de documento: " + this.documentTypeId)
       },
       error: (err) => {
         console.log(err)
@@ -57,10 +56,15 @@ export class UpdateCostumerComponent implements OnInit {
   updateUser() {
     const idNew = this.route.snapshot.paramMap.get("id")
     this.id = idNew !== null ? idNew : '';
-    console.log(this.documentTypeId)
     const updateU = new IUpdateUser(this.documentTypeId, this.document, this.fullName, this.email, this.phone, this.password)
     this.userService.updateUser(this.id, updateU).subscribe({
-      next: (data) => { console.log(data) },
+      next: (data) => {
+        Swal.fire({
+          title: "Hecho",
+          text: "Actualizacion Exitosa",
+          icon: "success"
+        })
+      },
       error: (err) => { console.log(err) },
       complete: () => { console.log("complete") }
     })
