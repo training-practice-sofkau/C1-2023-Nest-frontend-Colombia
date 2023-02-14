1a0ca9ceb4d2c6b7edd815675dac1ff7da9496c2
Javier Carrillo <javiercamur98@gmail.com>
2023-02-14 16:55:55 -0500

    Ajustes segundo formulario
    Se agregan las validaciones y funcionamiento del formulario de ModifyTodolist

--- src/app/modules/todo-list/services/todo-list.service.ts
+++ src/app/modules/todo-list/services/todo-list.service.ts
@@ -14,11 +14,19 @@ baseUrl: string = "https://localhost:7281/api/Todoitem";
 
 constructor(private readonly httpClient: HttpClient){}
 
-saveTodoList(newItem: AddItemModel): Observable<any>{
-  return this.httpClient.post(this.baseUrl, newItem);
-}
+  saveTodoList(newItem: AddItemModel): Observable<any>{
+    return this.httpClient.post(this.baseUrl, newItem);
+  }
 
-getAllTodoList(): Observable<ITodoItem[]>{
-  return this.httpClient.get<ITodoItem[]>(this.baseUrl);
-}
+  getAllTodoList(): Observable<ITodoItem[]>{
+    return this.httpClient.get<ITodoItem[]>(this.baseUrl);
+  }
+
+  getTodoListId(id: String): Observable<ITodoItem>{
+    return this.httpClient.get<ITodoItem>(this.baseUrl + "/" + id);
+  }
+
+  modifyTodoList(id: string, item: ITodoItem): Observable<ITodoItem>{
+    return this.httpClient.put<ITodoItem>(this.baseUrl + "/" + id, item);
+  }
 }
