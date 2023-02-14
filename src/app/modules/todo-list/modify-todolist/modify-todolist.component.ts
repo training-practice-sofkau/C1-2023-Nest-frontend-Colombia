bf367e4a49aa5fb82a010b072da60ba363636023
Javier Carrillo <javiercamur98@gmail.com>
2023-02-14 15:01:03 -0500

    se agrega el segundo formulario
    Se agrega el segundo formulario reactivo en el componente ModifyTodolist

--- src/app/modules/todo-list/modify-todolist/modify-todolist.component.ts
+++ src/app/modules/todo-list/modify-todolist/modify-todolist.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { TodoListService } from '../services/todo-list.service';
 
 @Component({
   selector: 'sofka-modify-todolist',
@@ -9,8 +11,16 @@ export class ModifyTodolistComponent {
 
   lista: string[];
 
-  constructor(){
+  frmFormularioModify : FormGroup;
+
+  constructor(private readonly todoListService: TodoListService){
     this.lista = ["../"]
+
+    this.frmFormularioModify = new FormGroup({
+      title: new FormControl(null, [Validators.required, Validators.maxLength(50)]),
+      description: new FormControl(null, Validators.required),
+      responsible: new FormControl(null, Validators.required)
+    });
   }
 
 }
