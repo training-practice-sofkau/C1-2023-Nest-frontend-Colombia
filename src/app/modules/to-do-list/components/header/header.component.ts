c957b56387bb9e165c11835e3be91d3655d0b203
Kevin Baquero <baquerochavarro@gmail.com>
2023-02-16 21:03:51 -0500

    Adding state service
    Adding state managenement for display username on header

--- src/app/modules/to-do-list/components/header/header.component.ts
+++ src/app/modules/to-do-list/components/header/header.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { AuthService } from '../../../security/security/userAuth/auth.service';
+import { StateService } from '../../../../shared/services/state.service';
 
 @Component({
   selector: 'sofka-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   routeDashboard: string[];
   routeAdd: string[];
-  constructor() {
+  routeList: string[];
+  state: boolean;
+  userName!: string | null;
+  constructor(
+    private readonly auth$: AuthService,
+    private readonly stateAvatar$: StateService
+  ) {
     this.routeDashboard = ['../dashboard'];
     this.routeAdd = ['add'];
+    this.routeList = ['list'];
+    this.state = this.stateAvatar$.State;
+  }
+  ngOnInit(): void {
+    if (this.state === true) {
+      this.userName = localStorage.getItem('userName');
+    }
+  }
+
+  logout(): void {
+    this.auth$.SignOut();
   }
 }
