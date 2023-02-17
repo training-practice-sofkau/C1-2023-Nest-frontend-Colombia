7e3e64f85725dfa116c2beb51c32348d66a6398d
Kevin Baquero <baquerochavarro@gmail.com>
2023-02-16 21:02:30 -0500

    Adding AngularFire
    Adding for routes protection

--- src/app/modules/main/app.module.ts
+++ src/app/modules/main/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './pages/app/app.component';
-import { IndexComponent } from './pages/index/index.component';
-import { MyPipePipe } from './pipes/my-pipe/my-pipe.pipe';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../../../environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [AppComponent, IndexComponent, MyPipePipe],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
