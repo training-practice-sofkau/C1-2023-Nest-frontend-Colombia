a56894c64d82a990db7e8eb1261d38c25fee9a49
migueltorresd <migueltorresdiaz1992@gmail.com>
2023-02-23 01:19:42 -0500

    Titulo: se agregan estilos Descripcion: se agregan estilos al proyecto

--- src/app/modules/main/app-routing.module.ts
+++ src/app/modules/main/app-routing.module.ts
@@ -15,16 +15,16 @@ const routes: Routes = [
     //carga perezosa del auth m=modulo
     path: 'auth',
     loadChildren: () => import('../auth/auth.module').then(m => m.AuthModule),
-    canActivate: [AngularFireAuthGuard],
-    data: { authGuardPipe: redirectLoggedInToDashboard },
+   // canActivate: [AngularFireAuthGuard],
+    //data: { authGuardPipe: redirectLoggedInToDashboard },
   },
   {
     //carga perezosa del main
     path: 'dashboard',
     loadChildren: () =>
       import('../dashoard/dashoard.module').then(m => m.DashoardModule),
-    canActivate: [AngularFireAuthGuard],
-    data: { authGuardPipe: redirectUnauthorizedToLogin },
+    //canActivate: [AngularFireAuthGuard],
+    //data: { authGuardPipe: redirectUnauthorizedToLogin },
   },
   {
     //DESPUES DEL LOCALHOST ME REDIRIGE A LA PAGINA DESEADA ** EN ESTE CASO MODULE AUTH
